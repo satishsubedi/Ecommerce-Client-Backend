258d940805ad93c2388934854e1344f87ec525c1
Shekhar Singh <smehla147@gmail.com>
2025-07-17 14:49:15 +1000

    fix some bug in filtrting

--- src/models/Product/ProductModel.js
+++ src/models/Product/ProductModel.js
@@ -6,5 +6,6 @@ export const getProductById = async (filter) => {
   return await productCollection.findById(filter);
 };
 export const getAllProducts = async () => await productCollection.find();
-export const getAllProductsByPath = async (filter) =>
-  await productCollection.find(filter);
+export const getAllProductsByPath = async (filter) => {
+  return await productCollection.find(filter);
+};
